92ee6ab1049d03cafe9378c852b3f6ee1e2da366
zhaoName <zhao1529835@126.com>
2019-11-26 18:03:37 +0800

    static list

--- DataStructures/ZZLinkedList/ZZStaticLinkedList.c
+++ DataStructures/ZZLinkedList/ZZStaticLinkedList.c
@@ -18,7 +18,7 @@ unsigned int _capaticy = 0;
 
 ZZStaticLinkedList* zz_init_staticLinkedList(unsigned int capacity)
 {
-    // the mix capacity is 3
+    // the min capacity is 3
     ZZStaticLinkedList *list = malloc(sizeof(ZZStaticLinkedList) * (capacity >= 3 ? capacity : 3));
     if (list) {
         for (int i=0; i<capacity; i++) {
@@ -31,6 +31,7 @@ ZZStaticLinkedList* zz_init_staticLinkedList(unsigned int capacity)
     return list;
 }
 
+#pragma mark -- CRDU
 
 void zz_addNode(ZZStaticLinkedList *list, void *newData)
 {
@@ -41,58 +42,139 @@ void zz_addNode(ZZStaticLinkedList *list, void *newData)
 /**
 * insert node at index in static linked list
 *
-* @param index position of node, [1, _capacity-1] in array, [0, _capacity-1] in list
+* @param index node position in the linked list, [0, _count-1] in list, [1, _capacity-2] in array
 */
 void zz_insertNodeAtIndex(ZZStaticLinkedList *list, void *newData, unsigned int index)
 {
     assert(list);
-    if (index > _count) return;
+    if (index > _count || _count + 2 > _capaticy) return;
     
     // get the index of the next node
     unsigned int nextIndex = list[0].cur;
     // record the cursor of the next node
     list[0].cur = list[nextIndex].cur;
+    
     // create new node
-    list[nextIndex].cur = 0;
     list[nextIndex].data = newData;
+    
     // lookup the index of previous node that given index
-    int preIndex = list[_capaticy-1].cur;
+    int preIndex = _capaticy-1;
     for (int i=0; i<index; i++) {
         preIndex = list[preIndex].cur;
     }
     // insert node at index
-    if (index != 0 || index != _count) {
-        list[nextIndex].cur = list[preIndex].cur;
-        list[preIndex].cur = nextIndex;
-    }
-    // reset the cursor of last element in array
-    if (index == 0) {
-        list[_capaticy-1].cur = nextIndex;
-    }
+    list[nextIndex].cur = list[preIndex].cur;
+    list[preIndex].cur = nextIndex;
     _count++;
 }
 
-
+/**
+* delete node at index in static linked list
+*
+* @param index node position in the linked list, [0, _count-1] in list, [1, _capacity-2] in array
+*/
 void zz_deleteNodeAtIndex(ZZStaticLinkedList *list, unsigned int index)
 {
     assert(list);
+    if (index >= _count) return;
+    
+    // lookup the index of previous node that given index
+    unsigned int nextIndex = _capaticy-1;
+    for (int i=0; i<index; i++) {
+        nextIndex = list[nextIndex].cur;
+    }
+    // lookup the index of the currnt node to be deleted
+    unsigned int delIndex = list[nextIndex].cur;
+    list[nextIndex].cur = list[delIndex].cur;
+    // free del node
+    list[delIndex].data = NULL;
+    list[delIndex].cur = list[0].cur;
+    // reset the index of next node
+    list[0].cur = delIndex;
+    _count--;
+}
+
+void zz_deleteNodeWithData(ZZStaticLinkedList *list, void *data)
+{
     
 }
 
+void zz_updateNodeAtIndex(ZZStaticLinkedList *list, void *newData, unsigned int index)
+{
+    assert(list);
+    if (index > _count) return;
+    
+    // lookup the index of the currnt node in array
+    unsigned int curIndex = _capaticy-1;
+    for (int i=0; i<=index; i++) {
+        curIndex = list[curIndex].cur;
+    }
+    list[curIndex].data = newData;
+}
 
+void* zz_getNodeDataAtIndex(ZZStaticLinkedList *list, unsigned int index)
+{
+    assert(list);
+    if (index > _count) return NULL;
+    
+    // lookup the index of the currnt node in array
+    unsigned int curIndex = _capaticy-1;
+    for (int i=0; i<=index; i++) {
+        curIndex = list[curIndex].cur;
+    }
+    return list[curIndex].data;
+}
+
+#pragma mark -- clear
+
+unsigned int zz_length_staticLinkedList(ZZStaticLinkedList *list)
+{
+    assert(list);
+    unsigned int length = 0;
+    unsigned int index = list[_capaticy - 1].cur;
+    while (index) {
+        index = list[index].cur;
+        length++;
+    }
+    return length;
+    //return _count;
+}
+
+void zz_clear_staticLinkedList(ZZStaticLinkedList *list)
+{
+    assert(list);
+    unsigned int index = _capaticy-1;
+    for (int i=0; i<_count; i++) {
+        index = list[_capaticy-1].cur;
+        list[_capaticy-1].cur = list[index].cur;
+        
+        list[index].data = NULL;
+        list[index].cur = index+1;
+    }
+    _count = 0;
+    list[0].cur = 1;
+}
+
+void zz_release_staticLinkedList(ZZStaticLinkedList **list)
+{
+    assert(*list);
+    _count = 0;
+    _capaticy = 0;
+    free(*list);
+}
+
+#pragma mark -- print
 
 
 void zz_print_staticLinkedList(ZZStaticLinkedList *list)
 {
     unsigned int index = list[_capaticy-1].cur;
     for (int i=0; i<_count; i++) {
-        printf("[%d-%d]->", *(int *)(list[index].data), index);
+        printf("[%d-%d]->", *(int *)(list[index].data), list[index].cur);
         index = list[index].cur;
     }
     printf("null\n");
     
     printf("nextNode:%d--firstNode:%d\n", list[0].cur, list[_capaticy-1].cur);
 }
-
-#pragma mark -- priate method
 
--- DataStructures/ZZLinkedList/ZZStaticLinkedList.h
+++ DataStructures/ZZLinkedList/ZZStaticLinkedList.h
@@ -12,7 +12,7 @@
 typedef struct ZZStaticLinkedList{
     // cursor: the subscript of the next node in the array.
     // cur = 0 indicates the next node is null.
-    // the cursor of the first elemnet in the array stores the subscript of the next node
+    // the cursor of the first element in the array stores the subscript of the next node
     // the cursor of the last element in the array stores the subscript of the first node
     // if the cursor of the last element in the array is 0, indicating that the static linked list is null
     unsigned int cur;
@@ -28,9 +28,25 @@ void zz_addNode(ZZStaticLinkedList *list, void *newData);
 /**
  * insert node at index in static linked list
  *
- * @param index position of node, [1, capacity-2] in array, [0, capacity-2] in list
+ * @param index node position int the linked list, [0, _count-1] in list, [1, _capacity-2] in array
  */
 void zz_insertNodeAtIndex(ZZStaticLinkedList *list, void *newData, unsigned int index);
+
+/**
+* delete node at index in static linked list
+*
+* @param index node position in the linked list, [0, _count-1] in list, [1, _capacity-2] in array
+*/
+void zz_deleteNodeAtIndex(ZZStaticLinkedList *list, unsigned int index);
+
+void zz_updateNodeAtIndex(ZZStaticLinkedList *list, void *newData, unsigned int index);
+void* zz_getNodeDataAtIndex(ZZStaticLinkedList *list, unsigned int index);
+
+
+
+unsigned int zz_length_staticLinkedList(ZZStaticLinkedList *list);
+void zz_clear_staticLinkedList(ZZStaticLinkedList *list);
+void zz_release_staticLinkedList(ZZStaticLinkedList **list);
 
 
 void zz_print_staticLinkedList(ZZStaticLinkedList *list);
