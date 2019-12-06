f837f6ba24109ad9c69c6dd0e512892618d85752
zhaoName <zhao1529835@126.com>
2019-12-06 11:46:22 +0800

    ZZArrayQueue annotate

--- DataStructures/ZZQueue/ZZArrayQueue.c
+++ DataStructures/ZZQueue/ZZArrayQueue.c
@@ -11,47 +11,64 @@
 #include <stdlib.h>
 #include <assert.h>
 
-unsigned int _maxSize = 0;
+// size of queue
+unsigned int _queueSize = 0;
 
+/**
+ * init circular queue with capacity(_queueSize = capacity)
+ *
+ * Enqueue and Dequeue time complexity is O(1)
+ * front == rear indicates that queue is empty
+ * (rear + 1) % _queueSize == front indicates that queue is full
+ */
 ZZArrayQueue* zz_init_arrayQueue(unsigned int capacity)
 {
     ZZArrayQueue *queue = malloc(sizeof(ZZArrayQueue));
     if (queue) {
         queue->front = queue->rear = 0;
         queue->data = malloc(sizeof(void*) * capacity);
-        _maxSize = capacity;
+        _queueSize = capacity;
     }
     return queue;
 }
 
 #pragma mark -- opreation
-
+/**
+ * insert new element in queue if it is not full, and modify queue->rear
+ *
+ * (rear + 1) % _queueSize == front indicates that queue is full
+ */
 void zz_addElem_arrayQueue(ZZArrayQueue *queue, void *newData)
 {
     assert(queue);
-    if ((queue->rear + 1) % _maxSize == queue->front) {
+    if ((queue->rear + 1) % _queueSize == queue->front) {
         printf("Queue is full !!\n");
         return;
     }
     queue->data[queue->rear] = newData;
-    queue->rear = (queue->rear + 1)  % _maxSize;
+    queue->rear = (queue->rear + 1)  % _queueSize;
 }
 
 void* zz_deleteElem_arrayQueue(ZZArrayQueue *queue)
 {
     assert(queue);
     if (queue->front == queue->rear) {
-        printf("Queue is empty !!n");
+        printf("Queue is empty !!\n");
         return NULL;
     }
     void *result = queue->data[queue->front];
-    queue->front = (queue->front + 1)  % _maxSize;
+    queue->data[queue->front] = NULL;
+    queue->front = (queue->front + 1)  % _queueSize;
     return result;
 }
 
 void* zz_getHeadElem_arrayQueue(ZZArrayQueue *queue)
 {
     assert(queue);
+    if (queue->front == queue->rear) {
+        printf("Queue is empty !!\n");
+        return NULL;
+    }
     return queue->data[queue->front];
 }
 
@@ -66,13 +83,13 @@ unsigned int zz_isEmpty_arrayQueue(ZZArrayQueue *queue)
 unsigned int zz_length_arrayQueue(ZZArrayQueue *queue)
 {
     assert(queue);
-    return (queue->rear - queue->front + _maxSize) % _maxSize;
+    return (queue->rear - queue->front + _queueSize) % _queueSize;
 }
 
 void zz_clear_arrayQueue(ZZArrayQueue *queue)
 {
     assert(queue);
-    for (int i=queue->front; i != queue->rear; i = (i + 1)  % _maxSize) {
+    for (int i=queue->front; i != queue->rear; i = (i + 1)  % _queueSize) {
         queue->data[i] = NULL;
     }
     queue->front = queue->rear = 0;
@@ -81,12 +98,13 @@ void zz_clear_arrayQueue(ZZArrayQueue *queue)
 void zz_release_arrayQueue(ZZArrayQueue **queue)
 {
     assert(*queue);
-    for (int i=(*queue)->front; i != (*queue)->rear; i = (i + 1)  % _maxSize) {
+    for (int i=(*queue)->front; i != (*queue)->rear; i = (i + 1)  % _queueSize) {
         (*queue)->data[i] = NULL;
     }
     free((*queue)->data);
     (*queue)->front = (*queue)->rear = 0;
     free(*queue);
+    *queue = NULL;
 }
 
 #pragma mark -- print
@@ -94,7 +112,7 @@ void zz_release_arrayQueue(ZZArrayQueue **queue)
 void zz_print_arrayQueue(ZZArrayQueue *queue)
 {
     printf("[");
-    for (int i=queue->front; i != queue->rear; i = (i + 1)  % _maxSize) {
+    for (int i=queue->front; i != queue->rear; i = (i + 1)  % _queueSize) {
         printf("%d, ", *(int*)queue->data[i]);
     }
     printf("]\n");
--- DataStructures/ZZQueue/ZZArrayQueue.h
+++ DataStructures/ZZQueue/ZZArrayQueue.h
@@ -19,16 +19,45 @@ typedef struct {
 }ZZArrayQueue;
 
 
-
+/**
+ * init circular queue with capacity(_queueSize = capacity)
+ *
+ * Enqueue and Dequeue time complexity is O(1)
+ * front == rear indicates that queue is empty
+ * (rear + 1) % _queueSize == front indicates that queue is full
+ */
 ZZArrayQueue* zz_init_arrayQueue(unsigned int capacity);
 
-
+/**
+ * insert new element in queue if it is not full, and modify queue->rear
+ *
+ * (rear + 1) % _queueSize == front indicates that queue is full
+ */
 void zz_addElem_arrayQueue(ZZArrayQueue *queue, void *newData);
+
+/**
+ * delete element at front in queue if it isn't empty, and modify front
+ *
+ * front == rear indicates that queue is empty
+ */
 void* zz_deleteElem_arrayQueue(ZZArrayQueue *queue);
+
+/**
+ * get queue head element
+ */
 void* zz_getHeadElem_arrayQueue(ZZArrayQueue *queue);
 
-
+/**
+ * front == rear indicates that queue is empty
+ */
 unsigned int zz_isEmpty_arrayQueue(ZZArrayQueue *queue);
+
+/**
+ * if rear > front, length = rear - front
+ * if rear < front, length = rear - front + _queueSize
+ *
+ * ==> length = (rear - front + _queueSize) % _queueSize
+ */
 unsigned int zz_length_arrayQueue(ZZArrayQueue *queue);
 void zz_clear_arrayQueue(ZZArrayQueue *queue);
 void zz_release_arrayQueue(ZZArrayQueue **queue);
